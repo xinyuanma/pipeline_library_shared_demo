9941c9301b938dd87d123406f79cd73b8b166bfd
xinyuan ma <xinyuan.ma@nokia.com>
2023-10-17 12:46:35 +0300

    remove argument from PAA

--- vars/print_arty_account.groovy
+++ vars/print_arty_account.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def PAA(stuff="world!")
+def PAA()
 {
     echo " * INFO :: ARTY_USERNAME        === ${ARTY_USERNAME}"
     echo " * INFO :: ARTY_PASSWORD        === ${ARTY_PASSWORD}"  
