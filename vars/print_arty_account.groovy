6c149da6ca6e0dee1b231e995db0aaab7e2cd9cf
xinyuan ma <xinyuan.ma@nokia.com>
2023-10-17 12:38:26 +0300

    add Print Arty Account method

---
+++ vars/print_arty_account.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def PAA(stuff="world!")
+{
+    echo " * INFO :: ARTY_USERNAME        === ${ARTY_USERNAME}"
+    echo " * INFO :: ARTY_PASSWORD        === ${ARTY_PASSWORD}"  
+}
+                        
+                        
